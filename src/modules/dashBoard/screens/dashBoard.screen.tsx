866966802dcb1f2f874ba8c1886800c124dcca0f
Anjana-13 <anjanavenkat1305@gmail.com>
2023-11-07 00:37:33 +0530

    add. style and icon for bottom tab

--- src/modules/dashBoard/screens/dashBoard.screen.tsx
+++ src/modules/dashBoard/screens/dashBoard.screen.tsx
@@ -4,14 +4,14 @@ import {
   RouteProp,
 } from '@react-navigation/native';
 import {Image, Text, View} from 'react-native';
-import {CoreButton, Header} from '../../../components';
+import {CoreButton, CoreIcon, Header} from '../../../components';
 import {navigationRef} from '../../../utils';
 import {styles} from '../styles/dashBoard.styles';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import OnBoardingScreen from '../../onboarding/screens/onBoarding.screen';
 import Home from './home.screen';
 import RecentActivity from './recent-activity.screen';
 import UserProfile from './profile.screen';
+
 
 export interface SignUpMainProps {
   navigation: NavigationProp<ParamListBase>;
@@ -41,7 +41,27 @@ const DashBoard = (props: SignUpMainProps) => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator initialRouteName="home">
+    <Tab.Navigator initialRouteName="home"
+    screenOptions={({ route }) => ({
+      tabBarActiveTintColor: '#000000',
+      tabBarShowLabel: false,
+      // tabBarStyle: styles.bottomTabStyle,
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName = '';
+        let routeName = route.name;
+
+        if (routeName === 'home') {
+          iconName = focused ? 'home' : 'home-outline';
+
+        } else if (routeName === 'recentActivity') {
+          iconName = focused ? 'time' : 'time-outline';
+        } 
+        else if (routeName === 'userProfile') {
+          iconName = focused ? "person-circle" : "person-circle-outline";
+        }
+        return <CoreIcon name={iconName} size={size} color={focused ? '#304FFE' : 'black'} />
+      },
+    })}>
       {tabScreenRoute?.map(route => {
         return (
           <Tab.Screen
