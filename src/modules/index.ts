d4d30501f85d37e112c8cbac263f591886d60380
Anjana-13 <anjanavenkat1305@gmail.com>
2023-11-07 00:44:03 +0530

    refactor index file for onboarding

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,29 +1,9 @@
 import OnBoardingStack from "./onboarding/navigation/onBoarding.navigation";
 import DashBoardStack from "./dashBoard/navigation/dashBoard.navigation";
-import SignUpMain from "./onboarding/screens/sign-up-main.screen";
-import EnterMobile from "./onboarding/screens/enter-mobile.screen";
-import ConfirmMobile from "./onboarding/screens/confirm-mobile.screen";
-import AddPersonalInfo from "./onboarding/screens/add-personal-info.screen";
-import PinSetup from "./onboarding/screens/pin-setup.screen";
-import Welcome from "./onboarding/screens/welcome.screen";
-import LogIn from "./onboarding/screens/log-in.screen";
-import AddCardBanner from "./onboarding/screens/add-card-banner.screen";
-import AddCard from "./onboarding/screens/add-card.screen";
-import VerifyCardOtp from "./onboarding/screens/verify-card-otp.screen";
-import CardList from "./onboarding/screens/card-list.screen";
+
 
 export {
     DashBoardStack,
     OnBoardingStack,
-    SignUpMain,
-    EnterMobile,
-    ConfirmMobile,
-    AddPersonalInfo,
-    PinSetup,
-    Welcome,
-    LogIn,
-    AddCardBanner,
-    AddCard,
-    VerifyCardOtp,
-    CardList
+
 }
---
+++ src/modules/onboarding/screens/index.ts
@@ -0,0 +1,25 @@
+import SignUpMain from './sign-up-main.screen';
+import EnterMobile from './enter-mobile.screen';
+import ConfirmMobile from './confirm-mobile.screen';
+import AddPersonalInfo from './add-personal-info.screen';
+import PinSetup from './pin-setup.screen';
+import Welcome from './welcome.screen';
+import LogIn from './log-in.screen';
+import AddCardBanner from './add-card-banner.screen';
+import AddCard from './add-card.screen';
+import VerifyCardOtp from './verify-card-otp.screen';
+import CardList from './card-list.screen';
+
+export {
+  SignUpMain,
+  EnterMobile,
+  ConfirmMobile,
+  AddPersonalInfo,
+  PinSetup,
+  Welcome,
+  LogIn,
+  AddCardBanner,
+  AddCard,
+  VerifyCardOtp,
+  CardList,
+};
