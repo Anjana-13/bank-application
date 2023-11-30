40aea02a66687dd7ae2e5e7d31cad355f322f499
Anjana-13 <anjanavenkat1305@gmail.com>
2023-11-30 19:29:42 +0530

    add flatlist of transaction items

--- src/modules/dashBoard/screens/home.screen.tsx
+++ src/modules/dashBoard/screens/home.screen.tsx
@@ -1,11 +1,50 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, FlatList} from 'react-native';
 import {styles} from '../styles/home.styles';
+import {CoreButton, CoreIcon} from '../../../components';
 const Home = () => {
+  const data = [
+    {id: '1', icon: 'card', title: 'Spending', amount: '$10'},
+    {id: '2', icon: 'cash', title: 'Income', amount: '$20'},
+    {id: '3', icon: 'newspaper', title: 'Bills', amount: '$30'},
+    {id: '4', icon: 'logo-usd', title: 'Savings', amount: '$40'},
+  ];
+
+  const renderItem = ({item}) => (
+    <View style={styles.item}>
+      <CoreIcon
+        name={item.icon}
+        iconStyle={styles.icon}
+        size={30}
+        color={'red'}
+      />
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.amount}>{item.amount}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.mainContainer}>
       <View style={styles.wrapper}>
         <View style={styles.topContainer}></View>
-        <View style={styles.bottomContainer}></View>
+        <View style={styles.bottomContainer}>
+          <View style={styles.transactionTitleView}>
+            <Text style={styles.transactionTitle}>Transaction</Text>
+            <TouchableOpacity>
+              <CoreIcon
+                name="arrow-forward-outline"
+                size={30}
+                color="#212721"
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.listContainer}>
+            <FlatList
+              data={data}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+            />
+          </View>
+        </View>
       </View>
       <View style={styles.divider}></View>
     </View>
--- src/modules/dashBoard/styles/home.styles.ts
+++ src/modules/dashBoard/styles/home.styles.ts
@@ -14,19 +14,58 @@ export const styles = StyleSheet.create({
   bottomContainer: {
     flex:65,
     position: 'relative',
+    marginHorizontal:20,
   },
   divider:{
     backgroundColor:'white',
     height:80,
     position: 'absolute',
     top:210,
-    left:40,
-    right:50,
+    left:20,
+    right:20,
     borderRadius:15,
   },
   wrapper:{
     flexDirection:'column',
     width:'100%',
     height:'100%',
+  },
+  listContainer:{
+    backgroundColor: 'white',
+    flex:1,
+    marginVertical:10,
+    borderRadius:10
+  },
+  transactionTitleView:{
+    height:30,
+    marginTop:50,
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+  transactionTitle:{
+    color:'black',
+    fontSize:20
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color:'red',
+    marginRight: 'auto', // Push amount to the right
+  },
+  amount: {
+    fontSize: 16,
+    color:'red',
   },
 })
